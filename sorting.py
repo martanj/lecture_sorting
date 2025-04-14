dd1b6868a4cbaf1dd6e5956165f3ddf0325b26ab
Martin Jiřikovský <271235@vutbr.cz>
2025-04-14 10:14:37 +0200

    dalsi commit

--- sorting.py
+++ sorting.py
@@ -48,7 +48,16 @@ def bubble_sort(number_array):
     return number_array
 
 
-
+def insertion_sort(number_array):
+    l = len(number_array)
+    for i in range(1, l):
+        key = number_array[i]
+        j = i - 1
+        while j >= 0 and number_array[j] > key:
+            number_array[j + 1] = number_array[j]
+            j = j - 1
+        number_array[j + 1] = key
+    return number_array
 
 
 def main():
@@ -56,6 +65,7 @@ def main():
     print(data)
     print(selection_sort(data["series_1"]))
     print(bubble_sort(data["series_2"]))
+    print(insertion_sort(data["series_3"]))
 
 
 if __name__ == '__main__':
