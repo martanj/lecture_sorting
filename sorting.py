f8e0033f4e1b8c620cea30f31637370ca8b6829d
Martin Jiřikovský <271235@vutbr.cz>
2025-04-14 08:45:56 +0200

    sorting algorithms

--- sorting.py
+++ sorting.py
@@ -1,4 +1,7 @@
+import csv
 import os
+
+from cviceni_7.hashovani import value
 
 
 def read_data(file_name):
@@ -10,11 +13,20 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
-
+    with open(file_path, 'r') as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = {}
+        for row in reader:
+            for header, value in row.items():
+                if header not in data:
+                    data[header] = [int(value)]
+                else:
+                    data[header].append(int(value))
+    return data
 
 def main():
-    pass
-
+    data = read_data("numbers.csv")
+    print(data)
 
 if __name__ == '__main__':
     main()
